516a06276b345ff32976289618755fdbd7b8f5cb
Eric City <eric.ciutad@hotmail.com>
2015-07-09 16:52:47 +0200

    Calculadora

--- app/src/main/java/com/example/ericcity/bandera/Bandera.java
+++ app/src/main/java/com/example/ericcity/bandera/Bandera.java
@@ -11,9 +11,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+import android.content.Intent;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 
 public class Bandera extends ActionBarActivity implements View.OnClickListener{
+
+
+    private TwitterLoginButton loginButton;
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "t3jsjzbaaj8RYthOgyhvD3R61";
+    private static final String TWITTER_SECRET = "tNcVT34QyB9w3p6S0SofdLXndT0c4YnchhhZd756br8egQxeqh";
+
 
     Button b;
     Button b2;
@@ -21,6 +38,8 @@ public class Bandera extends ActionBarActivity implements View.OnClickListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_bandera);
 
         b = (Button) findViewById(R.id.button1);
@@ -30,6 +49,23 @@ public class Bandera extends ActionBarActivity implements View.OnClickListener{
         b2.setOnClickListener(this);
         //b3 =(ImageButton) findViewById(R.id.snicker);
         //b3.setOnClickListener(this);
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // Do something with result, which provides a TwitterSession for making API calls
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Do something on failure
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        loginButton.onActivityResult(requestCode, resultCode, data);
     }
 
     @Override
